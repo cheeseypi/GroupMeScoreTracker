ad97b952b528b15c8ffe642b4bcc811853277cea
Matthew Doto <mvd369@gmail.com>
2018-02-28 23:27:32 -0500

    Current progress

--- scoretracker/tracker.py
+++ scoretracker/tracker.py
@@ -1,7 +1,7 @@
 import connexion
 import requests
 
-app = connexion.FlaskApp(__name__, specification_dir='api_spec/')
+app = connexion.FlaskApp(__name__, specification_dir='.')
 app.add_api('swagger.yaml')
 app.run(port=80)
 
