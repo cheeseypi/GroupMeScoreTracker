1a38a6ddb587a1a4930a33a7ded0b52cc1b02ed8
Matthew Doto <matt@mattdoto.com>
2018-03-06 16:08:54 -0500

    Added test cases

---
+++ scoretracker/__init__.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+app_dir = os.path.dirname(
+    os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__)))
+)
+sys.path.append(os.path.normpath(app_dir))
---
+++ scoretracker/test/test_server.py
@@ -0,0 +1,15 @@
+import unittest
+
+from unittest import mock
+
+from scoretracker import tracker
+
+class TestServer(unittest.TestCase):
+    def test_show_individual(self):
+        return True
+
+    def test_show_all(self):
+        return True
+    
+    def test_inc_score(self):
+        return True
