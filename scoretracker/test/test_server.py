86a685ea1ad6a417c5ab109b7e3120c7d5ff7e6a
Matthew Doto <matt@mattdoto.com>
2018-03-06 16:18:59 -0500

    started adding cases

--- scoretracker/test/test_server.py
+++ scoretracker/test/test_server.py
@@ -5,11 +5,19 @@ from unittest import mock
 from scoretracker import tracker
 
 class TestServer(unittest.TestCase):
-    def test_show_individual(self):
-        return True
+    @mock.patch('connexion.request.json', side_effect={'text': '/score show'})
+    @mock.patch('scoretracker.tracker.show_score', return_value=None)
+    def test_main_loop(self, shsc, connreq):
+        shsc.assert_called_once_with()
 
     def test_show_all(self):
         return True
-    
+
+    def test_show_all_individual(self):
+        return True
+
+    def test_show_one_individual(self):
+        return True
+
     def test_inc_score(self):
         return True
