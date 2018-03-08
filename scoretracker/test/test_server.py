d3f7bf8ff5b338b26dafbd0f1a09ded23aec92e4
Matthew Doto <matt@mattdoto.com>
2018-03-08 16:31:35 -0500

    Working tests!

--- scoretracker/test/test_server.py
+++ scoretracker/test/test_server.py
@@ -4,20 +4,37 @@ from unittest import mock
 
 from scoretracker import tracker
 
+
+class FakeArgAll():
+    json = {'text': '/score show'}
+class FakeArgAllForOne():
+    json = {'text': '/score show Ron'}
+class FakeArgOneForOne():
+    json = {'text': '/score show Ron Dissappointment'}
+class FakeArgIncr():
+    json = {'text': '/score Ron Dissappointment + 420'}
+
 class TestServer(unittest.TestCase):
-    @mock.patch('connexion.request.json', side_effect={'text': '/score show'})
+    @mock.patch('scoretracker.tracker.connexion.request', FakeArgAll)
     @mock.patch('scoretracker.tracker.show_score', return_value=None)
-    def test_main_loop(self, shsc, connreq):
+    def test_main_loop(self, shsc):
+        tracker.recv_msg()
         shsc.assert_called_once_with()
 
-    def test_show_all(self):
-        return True
+    @mock.patch('scoretracker.tracker.connexion.request', FakeArgAllForOne)
+    @mock.patch('scoretracker.tracker.show_score', return_value=None)
+    def test_show_all_individual(self, shsc):
+        tracker.recv_msg()
+        shsc.assert_called_once_with('Ron')
 
-    def test_show_all_individual(self):
-        return True
+    @mock.patch('scoretracker.tracker.connexion.request', FakeArgOneForOne)
+    @mock.patch('scoretracker.tracker.show_score', return_value=None)
+    def test_show_one_individual(self, shsc):
+        tracker.recv_msg()
+        shsc.assert_called_once_with('Ron', 'Dissappointment')
 
-    def test_show_one_individual(self):
-        return True
-
-    def test_inc_score(self):
-        return True
+    @mock.patch('scoretracker.tracker.connexion.request', FakeArgIncr)
+    @mock.patch('scoretracker.tracker.increment_score', return_value=None)
+    def test_inc_score(self, incsc):
+        tracker.recv_msg()
+        incsc.assert_called_once_with('Dissappointment', 'Ron', 420)
