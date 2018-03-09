1e53afb2113d7de5f8c9424118e0287afed1a31b
Matthew Doto <mvd369@gmail.com>
2018-03-09 10:55:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     packages=['scoretracker'],
     license='LICENSE.txt',
     description='A score tracker for GroupMe, allowing competition between friends',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     install_requires=open('requirements.txt').readlines()
 )
