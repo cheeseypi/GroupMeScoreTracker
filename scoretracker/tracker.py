78e0e3e24227d266a65489d73b26120a96f6ff09
Ed Minnix <egregius313@gmail.com>
2018-03-09 19:41:22 -0500

    The linter is happy now

--- scoretracker/tracker.py
+++ scoretracker/tracker.py
@@ -1,95 +1,164 @@
+"""
+The main logic of the bot.
+
+This file implements the database and bot command parsing.
+"""
+from collections import defaultdict
+import json
 import os
 
-import connexion
-import json
 import requests
+
+import connexion
+
 
 BOT_ID = os.environ['BOT_ID']
 DB_FILE = 'data.json'
+POST_URL = 'https://api.groupme.com/v3/bots/post'
+
+
+def defaultdict_of_ints(init=None):
+    """
+    Constructor for defaultdict(int)
+    """
+    return defaultdict(int, init or {})
+
+
+def get_db(filename=DB_FILE):
+    """
+    Try to load a JSON file and return a default dictionary containing
+    the scores for a given person.
+
+    If the file is not found or is not a valid JSON object, an empty
+    collection is returned.
+    """
+    try:
+        with open(filename, 'r') as db:
+            data = json.load(db)
+    except (FileNotFoundError, IOError, json.JSONDecodeError):
+        data = {}
+
+    return defaultdict(defaultdict_of_ints, data)
 
 
 def increment_score(score, person, number):
-    with open(DB_FILE, 'w+') as db:
-        try:
-            data = db.read()
-            database = json.loads(data)
-        except Exception:
-            database = {}
-        try:
-            database[person][score] += number
-        except KeyError as ke:
-            if person in str(ke):
-                database[person] = {}
-                database[person][score] = number
-            elif score in str(ke):
-                database[person][score] = number
-        db.write(str(database))
-    rT = {}
-    rT['bot_id'] = BOT_ID
-    rT['text'] = 'Incremented '+score+' score by '+str(number)+' for '+person
-    requests.post('https://api.groupme.com/v3/bots/post', json.dumps(rT))
+    """
+    Increments/decrements the score for the given user by number.
+
+    @ed> /score show ron disappointment
+    @scorebot> ron:
+                   disappointment: 420
+    @ed> /score ron disappointment + 420
+    @ed> /score show ron disappointment
+    @scorebot> ron:
+                   disappointment: 840
+    """
+    database = get_db()
+
+    database[person][score] += number
+
+    with open(DB_FILE, 'w') as db:
+        json.dump(database, db)
+
+    response = {
+        'bot_id': BOT_ID,
+        'text': 'Incremented {} score by {} for {}'.format(score, number, person)
+    }
+    requests.post(POST_URL, json.dumps(response))
+
+
+def get_scores(database, person, multi=True):
+    """
+    Return a string representing the scores a person has in the database.
+
+    The parameter `multi' is to specify whether the scores are used for
+    displaying the scores of a single person or multiple people.
+    """
+    indent = '     ' if multi else ''
+    return '\n'.join(
+        indent + '{score}: {points}'.format(score=score, points=points)
+        for score, points in database[person]
+    )
 
 
 def show_score(person=None, score=None):
-    rT = {}
-    rT['bot_id'] = BOT_ID
-    rT['text'] = ''
-    try:
-        with open(DB_FILE, 'r') as db:
-            try:
-                data = db.read()
-                database = json.loads(data)
-                if len(database) == 0:
-                    raise Exception()
-            except Exception:
-                rT['text'] = 'There are no scores yet!'
-                requests.post('https://api.groupme.com/v3/bots/post',
-                              json.dumps(rT))
-                return
-            if person is not None:
-                rT['text'] += person + ':\n'
-                if score is not None:
-                    rT['text'] += '    '+score+': '+database[person][score]+'\n'
-                else:
-                    for sco in database[person].keys():
-                        rT['text'] += '    '+sco+': '+database[person][sco]+'\n'
-            else:
-                for per, scores in database.items():
-                    rT['text'] += person + ':\n'
-                    for sco in scores:
-                        rT['text'] += '    '+sco+': '+database[per][sco]+'\n'
-    except Exception:
-        invalid_request()
-        return
-    requests.post('https://api.groupme.com/v3/bots/post', json.dumps(rT))
+    """
+    Sends to the chat the information about:
+    * All (user, score) combinations
+    * All scores of a given user
+    * A given user's score in a specific topic
+
+    @ed> /score show ron
+    @scorebot> ron:
+                   disappointment: 420
+                   understanding-the-rules: -69
+    """
+    response = {'bot_id': BOT_ID}
+    response['text'] = ''
+
+    database = get_db()
+
+    if not database:
+        # Backup did not exist or was empty
+        response['text'] = 'There are no scores yet!'
+
+    elif person is None:
+        response['text'] = '\n'.join(
+            '{person}:\n{scores}'.format(person=person, scores=get_scores(database, person))
+            for person in database
+        )
+
+    elif person not in database:
+        response['text'] = 'User is either unrecognized or has not points'
+
+    elif score is None:
+        response['text'] = get_scores(database, person, multi=False)
+
+    else:
+        response['text'] = '{person}:\n    {score}: {points}'.format(
+            person=person, score=score, points=database[person][score]
+        )
+
+    requests.post(POST_URL, json.dumps(response))
 
 
 def invalid_request():
-    rT = {}
-    rT['bot_id'] = BOT_ID
-    rT['text'] = 'That was not a valid scorebot command'
-    requests.post('https://api.groupme.com/v3/bots/post', json.dumps(rT))
+    """
+    Respond to the chat informing the user that they entered an invalid
+    command for the bot.
+    """
+    response = {
+        'bot_id': BOT_ID,
+        'text': 'That was not a valid scorebot command'
+    }
+    requests.post(POST_URL, json.dumps(response))
 
 
-# /score <person> <score> <+/-> <#>
-# /score show
-# /score show <person> [score]
 def recv_msg():
+    """
+    Handles the CRUD operations represented by the following commands:
+
+    Increment/Decrement a score:
+        /score <person> <score> <+/-> <#>
+
+    Show all user's scores:
+        /score show
+
+    Show a specific person's score(s):
+        /score show <person> [score]
+    """
     message = connexion.request.json['text']
     message = message.split(' ')
     if message[0] == '/score':
         if len(message) == 5:
             print("Change scores")
-            score = message[2]
-            person = message[1]
-            number = int(message[4])
-            if message[3] == '-':
+            _, person, score, plus_minus, number = message
+            if plus_minus == '-':
                 number *= -1
             increment_score(score, person, number)
         elif len(message) == 4:
             print("Show one score for one person")
-            score = message[3]
-            person = message[2]
+            _, _, person, score = message
             show_score(person, score)
         elif len(message) == 3:
             print("Show all scores for one person")
