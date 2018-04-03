48f525c557239979fde8f0af675f8ff5fc15d3c8
Matthew Doto <matt@mattdoto.com>
2018-04-03 11:13:38 -0400

    Working installation from pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='GroupMeScoreTracker',
-    version='1.1',
+    version='1.3',
     url='https://github.com/cheeseypi/GroupMeScoreTracker',
     download_url='https://github.com/cheeseypi/GroupMeScoreTracker/archive/v1.0.tar.gz',
     author='Matthew Doto',
@@ -11,5 +11,6 @@ setup(
     license='LICENSE.txt',
     description='A score tracker for GroupMe, allowing competition between friends',
     long_description=open('README.rst').read(),
-    install_requires=open('requirements.txt').readlines()
+    install_requires=open('requirements.txt').readlines(),
+    include_package_data=True
 )
